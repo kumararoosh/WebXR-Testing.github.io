3dd996ed9ac67ff238af51c02a4a1b2702cbba7c
Aroosh Kumar <arkumar@cs.washington.edu>
2022-01-18 17:58:46 -0800

    removed light

--- HTMLImpl/index.js
+++ HTMLImpl/index.js
@@ -21,8 +21,8 @@ var createXRScene = async function () {
     var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
     camera.setTarget(BABYLON.Vector3.Zero());
     camera.attachControl(canvas, true);
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
+    // var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    // light.intensity = 0.7;
     var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
     sphere.position.y = 1;
 
